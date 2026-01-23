90b0a4117ece143de51c315a46a59da4f10cc12d
Harold Rajaonarison <rnarsonharold@gmail.com>
2026-01-23 02:51:32 +0100

    try to use fpdart package

--- lib/features/events/domain/repositories/event_repository.dart
+++ lib/features/events/domain/repositories/event_repository.dart
@@ -1,6 +1,6 @@
 import 'package:camabelcommunity/features/events/domain/entities/event.dart';
 
-abstract class EventRepository {
+abstract interface class EventRepository {
   Future<List<Event>> getEvents();
   Future<Event?> getEventById(String id);
 }
--- lib/features/events/domain/usecases/get_events.dart
+++ lib/features/events/domain/usecases/get_events.dart
@@ -1,10 +1,18 @@
+import 'package:camabelcommunity/core/error/failure.dart';
+import 'package:camabelcommunity/core/usecase/usecase.dart';
 import 'package:camabelcommunity/features/events/domain/entities/event.dart';
 import 'package:camabelcommunity/features/events/domain/repositories/event_repository.dart';
+import 'package:fpdart/fpdart.dart';
 
-class GetEvents {
+class GetEvents implements UseCase<List<Event>, NoParams> {
   final EventRepository eventRepository;
 
   GetEvents(this.eventRepository);
+
+  @override
+  Future<Either<Failure, List<Event>>> call(NoParams params) {
+    return eventRepository.getEvents() ;
+  }
 
   Future<List<Event>> call() {
     return eventRepository.getEvents();
