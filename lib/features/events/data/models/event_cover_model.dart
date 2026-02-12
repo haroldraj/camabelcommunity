969e293c86bbc6237bc5472db2e2709d489c03fd
Harold Rajaonarison <rnarsonharold@gmail.com>
2026-02-12 01:37:55 +0100

    working mock data ingestion to firestore

--- lib/features/events/data/models/event_cover_model.dart
+++ lib/features/events/data/models/event_cover_model.dart
@@ -4,5 +4,23 @@ class EventCoverModel {
   final String? url;
   final String? path;
 
-  EventCoverModel({required this.id, required this.source, this.url, this.path});
+  EventCoverModel({
+    required this.id,
+    required this.source,
+    this.url,
+    this.path,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {"id": id, "source": source, "url": url, "path": path};
+  }
+
+  factory EventCoverModel.fromJson(String id, Map<String, dynamic> json) {
+    return EventCoverModel(
+      id: id,
+      source: json["source"],
+      url: json["url"],
+      path: json["path"],
+    );
+  }
 }
